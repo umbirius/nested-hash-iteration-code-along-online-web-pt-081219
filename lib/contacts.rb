509999bf61469baef84722209ba3a388726f377d
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 19:02:53 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,15 +14,20 @@ require 'pry'
     }
   }
   
-# def remove_strawberry(contacts)
-# contacts["Freddy Mercury"].each do |attribute, value]
-#     if attribute == [favorite_ice_cream_flavors]
-#     value.each do |ice_cream|
-#       if ice_cream =="strawberry"
-#         contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete("strawberry")
+def remove_strawberry(contacts)
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury" 
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+          binding.pry
+      end
+   end 
+  end 
+end 
        
        
-# end
+end
 
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury" 
