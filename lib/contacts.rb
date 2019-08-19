a9247a84ffb329e34d1bb818fddcac3e8c624cb2
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 18:58:13 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -25,5 +25,7 @@ require 'pry'
 # end
 
 contacts.each do |person, contact_details_hash|
-  binding.pry
+  if person = ="Freddy Mercury" 
+    contact_details_hash.each do |attribute, data
+     binding.pry
 end 
