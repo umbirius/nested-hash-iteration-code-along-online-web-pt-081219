2711e4be340b0a0fb5dd950e7a844b189e1d52f2
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 19:03:08 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,7 +20,6 @@ contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
-          binding.pry
       end
    end 
   end 
@@ -34,7 +33,6 @@ contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
-          binding.pry
       end
    end 
   end 
